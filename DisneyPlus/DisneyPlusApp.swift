6ddd6cb377a93200247ceb28520564485f470ac9
Nur Hidayat <hidayat.speed78@gmai.com>
2021-07-06 00:09:34 +0700

    Initial Commit

---
+++ DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by User on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
